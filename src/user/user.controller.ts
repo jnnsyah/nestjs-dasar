de6f39d7226399e4a5fd2e06d306f57a58c32857
jiansyah <jiansyah1248@gmail.com>
2025-09-01 16:17:28 +0700

    HTTP Response Decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import express from 'express';
 
 @Controller('/api/users')
 export class UserController {
@@ -18,8 +27,40 @@ export class UserController {
     return `Hi ${name || 'stranger'}, you're ${age || 0}`;
   }
 
-  @Get('/:id')
-  getById(@Param('id') id: string): string {
-    return `GET ${id}`;
+  // @Get('/:id')
+  // getById(@Param('id') id: string): string {
+  //   return `GET ${id}`;
+  // }
+
+  //HTTP Response
+  @Get('/sample-response')
+  sampleResponse(@Res() response: express.Response) {
+    response.status(200).send('Sample Response');
+  }
+
+  // HTTP response decorator
+  @Get('/response-decorator')
+  @Header('content-type', 'application/json')
+  @HttpCode(200)
+  responseDecorator(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  // @Get('/redirect')
+  // @Redirect()
+  // redirect(): HttpRedirectResponse {
+  //   return {
+  //     url: '/api/users/response-decorator',
+  //     statusCode: 301,
+  //   };
+  // }
+
+  @Get('/flag')
+  @Header('flag', 'Flag-00{Flag CTF ceritanya')
+  @HttpCode(404)
+  getFlag(): string {
+    return 'Flag';
   }
 }
